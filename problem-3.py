3ead5d5816ac4de069eb155fd7684c1dd9bd2448
venkateswari-123 <168544320+venkateswari-123@users.noreply.github.com>
2025-09-02 11:58:58 +0530

    Update problem-3.py

--- problem-3.py
+++ problem-3.py
@@ -1,10 +1,8 @@
+#python programming language
 def generate_series(a):
     # If 'a' is even, generate (a-1) odd numbers
     count = a if a % 2 != 0 else a - 1
     series = [2 * i + 1 for i in range(count)]
     return series
-
-
-# Example usage:
 a = int(input("Enter a number (a): "))
 print("Output:", ", ".join(map(str,generate_series(a))))
