1c353788488d8e84bec33299f0a4c10f4ec29be7
venkateswari-123 <168544320+venkateswari-123@users.noreply.github.com>
2025-09-02 11:58:00 +0530

    Update problem-2.py

--- problem-2.py
+++ problem-2.py
@@ -1,10 +1,8 @@
+#python programming language
 def generate_series(a):
     series = []
     for i in range(a):
-        series.append(2 * i + 1)  # formula for odd numbers
+        series.append(2 * i + 1)  # formula for odd number
     return series
-
-
-# Example usage:
 a = int(input("Enter a number (a): "))
 print("Output:", ", ".join(map(str,generate_series(a))))
