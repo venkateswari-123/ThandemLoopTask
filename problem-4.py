d06626c729eb2324e496ab1590bdc3b580a1310f
venkateswari-123 <168544320+venkateswari-123@users.noreply.github.com>
2025-09-02 11:59:40 +0530

    Update problem-4.py

--- problem-4.py
+++ problem-4.py
@@ -1,11 +1,9 @@
+#python programming language
 def count_multiples(numbers):
     result = {}
-    for i in range(1, 10):  # from 1 to 9
+    for i in range(1, 10): 
         count = sum(1 for num in numbers if num % i == 0)
         result[i] = count
     return result
-
-
-# Example usage:
 numbers = [1, 2, 8, 9, 12, 46, 76, 82, 15, 20, 30]
 print(count_multiples(numbers))
